45e235a65efc14a6799c5e7ed3f916a0e2384423
iafar86 <iafar86@hotmail.com>
2015-04-28 09:39:29 -0300

    Probando subida Git con clases Empleador.cs y Pais.cs

---
+++ VLaboral_admin/Models/Empleador.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace VLaboral_admin.Models
+{
+    public class Empleador
+    {
+        public int Id { get; set; }
+        public string Cuit { get; set; }
+        public string Rsocial { get; set; }
+        public string Dir { get; set; }
+        public string Tel { get; set; }
+        public string Mail { get; set; }
+        public string MyProperty { get; set; }
+
+
+    }
+}
---
+++ VLaboral_admin/Models/Pais.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace VLaboral_admin.Models
+{
+    public class Pais
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Idioma { get; set; }
+        public string ZHoraria { get; set; }
+
+        //Relacion 1 a muchos
+        public int LocalidadId { get; set; }
+        public virtual Localidad Localidad { get; set; }
+    }
+
+    public class Localidad
+    {
+        
+    }
+}
